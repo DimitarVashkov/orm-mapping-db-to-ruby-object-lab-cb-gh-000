aa1e8872a193ec0620a09c27f470ca63633711ac
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-31 20:02:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
-    
+
   end
 
   def self.find_by_name(name)
@@ -31,10 +31,18 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-
-    # find the student in the database given a name
-    # return a new instance of the Student class
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
 
   def save
     sql = <<-SQL
